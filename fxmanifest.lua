e389ecf58e914f6a7a947cc0a7f9a30d182d2e86
reverseHaze <96739561+reverseHaze@users.noreply.github.com>
2023-02-21 01:45:26 +0000

    **CHANGES** -changed __resource.lua into fxmanifest.lua -updated function in client/main.lua to work with latest ESX -updated function in server/main.lua to work with latest ESX -updated sql (limit no longer used > replaced with weight)
    **ADDED**
    +Translation to german added

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,1 +1,21 @@
 client_scripts {
+  '@es_extended/locale.lua',
+  'locales/fr.lua',
+  'locales/ru.lua',
+  'locales/en.lua',
+  'locales/tr.lua',
+  'config.lua',
+  'client/main.lua'
+}
+
+server_scripts {
+  '@es_extended/locale.lua',
+  'locales/fr.lua',
+  'locales/ru.lua',
+  'locales/en.lua',
+  'locales/tr.lua',
+  'config.lua',
+  'server/main.lua'
+}
+
+shared_script '@es_extended/imports.lua'
---
+++ locales/de.lua
@@ -0,0 +1,8 @@
+Locales['de'] = {
+    ['hotdog_shop_prompt'] 	= '~INPUT_PICKUP~ um einen ~y~Hot Dog~w~ zu bestellen.',
+    ['bought']             	= '~r~Vorsicht heiß!~w~ Das macht bitte ~o~',
+    ['not_enough']   			= 'Du hast nicht genügend Geld dabei.',
+    ['max_item']           	= 'Ich habe keine Hot Dogs mehr..',
+    ['command_in_progress'] 	= 'Deine ~o~Bestellung~s~ wird ~r~bearbeitet~s~.',
+    ['used_hotdog']			= 'Du isst einen ~b~ Hot Dog.',
+  }
